6f3d9fdf2a837cee19e47bc4a4bf08da7148d89c
Adil <aelabedelalaoui@acm.org>
2020-09-21 10:37:02 +0200

    Base components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Card } from "./Card";
+import { Column } from "./Column";
 import { AppContainer } from './styles'
 
 function App() {
   return (
     <AppContainer>
-      Columns will go here
+      <Column text={"TO DO"}>
+        <Card text={"Generate app data"} />
+      </Column>
+      <Column text={"In Progress"}>
+        <Card text={"Learn Typescript"} />
+      </Column>
+      <Column text={"Done"}>
+        <Card text={"Begin to use static typing"} />
+      </Column>
     </AppContainer>
   );
 }
